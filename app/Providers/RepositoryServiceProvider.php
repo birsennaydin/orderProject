b770062c21110ae7951f1d8451c1a9da7334f479
birsenaydin <bsnaydin@gmail.com>
2022-09-21 14:19:25 +0300

    register service

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Interfaces\DiscountRepositoryInterface;
 use App\Interfaces\OrderRepositoryInterface;
 use App\Interfaces\ProductRepositoryInterface;
+use App\Repositories\DiscountRepository;
 use App\Repositories\OrderRepository;
 use App\Repositories\ProductRepository;
 use Illuminate\Support\ServiceProvider;
@@ -19,6 +21,8 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
         $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+        $this->app->bind(DiscountRepositoryInterface::class, DiscountRepository::class);
+
     }
 
     /**
